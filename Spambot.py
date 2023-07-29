61db7351dcd2225bff1e2238897feb5dffb5f17c
Mohammed <123330279+mohaammed17@users.noreply.github.com>
2023-07-29 14:36:09 +0100

    Update Spambot.py
    1) edited outdated xpaths with new ones 
    2) new method (find by xpath) of selenium added after update 
    3) more efficient and comments added

--- Spambot.py
+++ Spambot.py
@@ -1,17 +1,35 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 driver = webdriver.Chrome("C:/Users/mohammed/Downloads/chromedriver_win32/chromedriver.exe")
 driver.get("https://web.whatsapp.com/")
 driver.maximize_window()
-name=input("enter the name here:")
-msg=input("enter the message here:")
-count=int(input("enter the count here:"))
-input("enter anything after scanning QR code")
 
-user = driver.find_element_by_xpath("//span[@title='{}']".format(name))
-user.click()
-msg_box= driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]")
-for index in range(count):
-    msg_box.send_keys(msg)
-    driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[3]/button").click()
+# Wait for the user to scan the QR code and proceed manually
+input("Enter anything after scanning QR code and waiting for WhatsApp to load")
 
-print("success")  
+name = input("Enter the name here: ")
+msg = input("Enter the message here: ")
+count = int(input("Enter the count here: "))
+
+try:
+    user = driver.find_element(by=By.XPATH, value="//span[@title='{}']".format(name))
+    user.click()
+
+    for index in range(count):
+        msg_box = driver.find_element(by=By.XPATH, value="//*[@id='main']/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p")
+        msg_box.send_keys(msg)
+
+        # Give a small delay before clicking send to avoid WhatsApp detecting it as spam
+        time.sleep(0.5)
+
+        # Click the send button to send the message
+        send_button = driver.find_element(by=By.XPATH, value="//*[@id='main']/footer/div[1]/div/span[2]/div/div[2]/div[2]/button/span")
+        send_button.click()
+
+    print("Success")
+except Exception as e:
+    print("Error:", e)
+finally:
+    driver.quit()
